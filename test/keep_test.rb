530c296d4112e796d45bcae701bebefacb3e2915
Evgeny Peleh <pelehev@gmail.com>
2019-11-01 17:37:19 +0600

    Add keep test

--- test/keep_test.rb
+++ test/keep_test.rb
@@ -3,4 +3,23 @@
 require_relative 'global'
 
 class KeepTest < Test::Unit::TestCase
+  def test_keep_compress
+    tmp = Tempfile.new.tap(&:close).path
+    assert_false File.exist?(tmp + '.' + APP_NAME)
+
+    assert_equal 0, `#{EXEC} --keep #{tmp}; printf $?`.to_i
+    assert File.exist?(tmp)
+    assert File.exist?(tmp + '.' + APP_NAME)
+  end
+
+  def test_keep_decompress
+    tmp = Tempfile.new.tap(&:close).path
+    assert_equal 0, `#{EXEC} #{tmp}; printf $?`.to_i
+
+    assert_false File.exist?(tmp)
+    assert_equal 0, `#{EXEC} -dk #{tmp + '.' + APP_NAME}; printf $?`.to_i
+
+    assert File.exist?(tmp)
+    assert File.exist?(tmp + '.' + APP_NAME)
+  end
 end
